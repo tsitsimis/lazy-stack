b4741891ac11d5ab8f7c0470630240f99d62bf2a
Theodore Tsitsimis <th.tsitsimis@gmail.com>
2021-02-13 13:15:20 +0200

    Ignore sqlite

--- examples/create_sql_tables/app.py
+++ examples/create_sql_tables/app.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-from lazystack.core import LazyStack
+from lazystack import LazyStack
 
 
 # Initialize with Great Expectations
