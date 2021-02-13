f8ac52718b54004ad12eb0e386694712186c751d
Theodore Tsitsimis <th.tsitsimis@gmail.com>
2021-02-13 19:08:17 +0200

    Rename example

---
+++ examples/lazy-example/app.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+from lazystack import LazyStack
+
+
+# Initialize with Great Expectations
+stack = LazyStack(ge_dir="./great_expectations/expectations")
+
+# Create SQLAlchemy models
+stack.create_sqla_models()
+engine = create_engine("sqlite:///./db/sqlite.db")
+stack.metadata.drop_all(engine, checkfirst=False)
+stack.metadata.create_all(engine, checkfirst=False)
+
+# Create Pydantic models and fastAPI CRUD routes
+stack.create_pydantic_models()
+fastapi_app = stack.create_crud_routers(engine=engine)
